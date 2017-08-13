62bbddef636c268f9c567ba44d1ea919152cc91f
Christian Gaser <christian.gaser@uni-jena.de>
2017-08-13 15:38:33 +0000

    Changed paths: M       CHANGES.txt M       tfce_mesh.m
    Fixed: Removed peak error in tfce_mesh.m
    
    
    git-svn-id: svn://141.35.69.214/tfce8@123 8e3dde7d-7981-41eb-959a-5c577105cc28

--- tfce_mesh.m
+++ tfce_mesh.m
@@ -16,14 +16,11 @@ n_steps = ceil(t_max/dh);
 %--------------------------------------------------------------------------
 A       = spm_mesh_adjacency(faces);
 A       = A + speye(size(A));
-A1 = A;
-A2 = A;
+A1 = A; A2 = A;
 
 clear A
 
-t1 = t;
-t2 = t;
-clear t
+t1 = t; t2 = t;
 
 for j = 1:n_steps
 
@@ -32,17 +29,19 @@ for j = 1:n_steps
   % positive t-values
   T   = (t1 >= thresh);
   ind = find(T);
+  T1 = (t >= thresh);
+  ind1 = find(T1);
   
   if ~isempty(ind)
-  
     [C, A1] = find_connected_component(A1, T);
     C = C(ind);
     
     for i = 1:max(C)
       M = find(C == i);
       k = length(M);
-      tfce(ind(M)) = tfce(ind(M)) + power(k,E)*power(thresh,H)*T(ind(M));
+      tfce(ind1(M)) = tfce(ind1(M)) + power(k,E)*power(thresh,H)*T1(ind1(M));
     end
+    
     t1 = t1(ind);  
   end
   
@@ -50,6 +49,8 @@ for j = 1:n_steps
   if min(t2(:)) < 0
     T   = (-t2 >= thresh);
     ind = find(T);
+    T2   = (-t >= thresh);
+    ind2 = find(T2);
   
     if ~isempty(ind)
       [C, A2] = find_connected_component(A2, T);
@@ -58,17 +59,17 @@ for j = 1:n_steps
       for i = 1:max(C)
         M = find(C == i);
         k = length(M);
-        tfce(ind(M)) = tfce(ind(M)) - power(k,E)*power(thresh,H)*T(ind(M));
+        tfce(ind2(M)) = tfce(ind2(M)) - power(k,E)*power(thresh,H)*T2(ind2(M));
       end
+      
     end
-
     t2 = t2(ind);  
   end
 
 end
 
 %--------------------------------------------------------------------------
-function [C, A1] = find_connected_component(A, T)
+function [C, A] = find_connected_component(A, T)
 % find connected components 
 % FORMAT C = find_connected_component(A,T)
 % A        - a [nxn[ (reduced) adjacency matrix
@@ -86,15 +87,14 @@ if ~islogical(T)
   T   = ~isnan(T);
 end
   
-A1 = A;
-A1(~T,:) = [];
-A1(:,~T) = [];
+A(~T,:) = [];
+A(:,~T) = [];
 
 %-And perform Dulmage-Mendelsohn decomposition to find connected components
 %--------------------------------------------------------------------------
-[p,q,r] = dmperm(A1);
+[p,q,r] = dmperm(A);
 N       = diff(r);
-CC      = zeros(size(A1,1),1);
+CC      = zeros(size(A,1),1);
 for i = 1:length(r)-1
   CC(p(r(i):r(i+1)-1)) = i;
 end

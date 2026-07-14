fe720b2c78641846e283de0e44b3faf9662bfa4e
Christian Gaser <christian.gaser@uni-jena.de>
2026-07-14 20:15:56 +0200

    Set toolbox version to r310 in docs
    Align version references in `Contents.m`, `INSTALL.txt`, and `html/tfce.html` from r317 to r310 for the 2026-07-14 TFCE1.3 release metadata.

--- Contents.m
+++ Contents.m
@@ -1,5 +1,5 @@
 % TFCE Toolbox
-% Version  317  (TFCE1.3) 2026-07-14
+% Version  310  (TFCE1.3) 2026-07-14
 % __________________________________________________________________________
 % Copyright (C) 2020 Christian Gaser christian.gaser@uni-jena.de
 %
